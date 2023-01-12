bbd0c470a9c21fbb93f0ead7b6aad97cd4668972
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 21:54:31 -0800

    -> var 2 input value & setter to event handler

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Editor.css';
 
-export default function Editor() {
+export default function Editor({ name, setName }) {
+  const handleChange = (e) => {
+    e.target.name === 'name' && setName(e.target.value);
+  };
   return (
     <div className="editor-container">
       <h1>Character Editor</h1>
       <label htmlFor="name">
         Name:
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" value={name} onChange={handleChange} />
       </label>
       <label>
         Head:
