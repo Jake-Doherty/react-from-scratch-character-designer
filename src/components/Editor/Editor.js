55edc77ef8830dfdf7b1f0bcf4c5ba47f3dc5411
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 18:19:06 -0800

    add editor inputs and selects

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './Editor.css';
 
 export default function Editor() {
   return (
-    <div>
-      <h1>Editor</h1>
+    <div className="editor-container">
+      <h1>Character Editor</h1>
+      <label htmlFor="name">
+        Name:
+        <input type="text" id="name" name="name" />
+      </label>
+      <label>
+        Head:
+        <select></select>
+      </label>
+      <label>
+        Body:
+        <select></select>
+      </label>
+      <label>
+        Legs:
+        <select></select>
+      </label>
     </div>
   );
 }
