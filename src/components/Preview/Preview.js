c96936a7c6b7d84f57e82431f99de98767eba149
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 22:46:53 -0800

    -> state var to file path to retrieve file

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Preview.css';
 
-export default function Preview({ name, head, body }) {
+export default function Preview({ name, head, body, legs }) {
   return (
     <>
       <div className="character-preview">
@@ -10,10 +10,7 @@ export default function Preview({ name, head, body }) {
         <div className="character-image-container">
           <img src={`/images/${head}.jpg`} alt={head} />
           <img src={`/images/${body}.png`} alt={body} />
-          <img
-            src="https://www.seekpng.com/png/detail/76-766707_close-legs-and-shoes-clipart.png"
-            alt="legs"
-          />
+          <img src={`/images/${legs}.png`} alt={legs} />
         </div>
       </div>
       <div className="character-catchphrases">
