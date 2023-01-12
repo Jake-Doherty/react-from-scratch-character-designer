17956e1b5f26318d0e91cffbf7e8ae48aa8f3f93
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-11 21:53:01 -0800

    import useState -> var & setter to components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 import Editor from '../Editor/Editor.js';
 import Preview from '../Preview/Preview.js';
 import './Home.css';
 
 export default function Home() {
+  const [name, setName] = useState('Character Name');
   return (
     <main>
-      <Editor />
-      <Preview />
+      <Editor name={name} setName={setName} />
+      <Preview name={name} />
     </main>
   );
 }
