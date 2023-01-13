fb0f01892d9e6f19fb036ccffb938f7ae77cece5
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-12 18:21:28 -0800

    map through catchphrases and increment index that is set to key

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './Preview.css';
 
-export default function Preview({ name, head, body, legs, headCount, bodyCount, legsCount }) {
+export default function Preview({
+  name,
+  head,
+  body,
+  legs,
+  headCount,
+  bodyCount,
+  legsCount,
+  catchPhrases,
+}) {
   return (
     <>
       <div className="character-preview">
@@ -17,7 +26,14 @@ export default function Preview({ name, head, body, legs, headCount, bodyCount, 
         <h1>Catchphrases & Stats</h1>
         <ul className="character-catchphrases">
           {/* map through catch phrases and return li */}
-          lorem ipsum dolor sit
+          {catchPhrases.map((phrase, i) => {
+            i += 1;
+            return (
+              <li id={i} key={i}>
+                {phrase}
+              </li>
+            );
+          })}
         </ul>
         <div className="selection-count">
           <p className="head-count">Head has been selected {headCount} times.</p>
